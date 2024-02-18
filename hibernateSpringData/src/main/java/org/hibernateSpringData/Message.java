49e65dcd1c77a484bd8a04fb6e768d298cbc9e7d
ShrayankM <shraymist@gmail.com>
2024-02-18 10:04:52 +0530

    added Message entity to store and load text from DB

---
+++ hibernateSpringData/src/main/java/org/hibernateSpringData/Message.java
@@ -0,0 +1,35 @@
+package org.hibernateSpringData;
+
+
+import lombok.*;
+import org.hibernate.Hibernate;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Entity
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
+public class Message {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    private String text;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+        Message message = (Message) o;
+        return id != null && Objects.equals(id, message.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
+}
