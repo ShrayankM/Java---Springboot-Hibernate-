1a0e1047bc3ea07dc059b220993a1439462c9c57
ShrayankM <shraymist@gmail.com>
2024-02-18 10:07:18 +0530

    added basic test for Spring-data hibernate

--- hibernateSpringData/src/test/java/Tests.java
+++ hibernateSpringData/src/test/java/Tests.java
@@ -1,2 +1,37 @@
-package PACKAGE_NAME;public class Tests {
+import org.hibernateSpringData.Message;
+import org.hibernateSpringData.SpringDataConfiguration;
+import org.hibernateSpringData.repositories.MessageRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = { SpringDataConfiguration.class })
+public class Tests
+{
+    @Autowired
+    private MessageRepository messageRepository;
+
+    @Test
+    public void storeLoadMessage() {
+        Message message = new Message();
+        message.setText("Hello World from Spring Data JPA");
+
+        messageRepository.save(message);
+
+        List<Message> messageList = (List<Message>) messageRepository.findAll();
+
+        assertAll(
+                () -> assertEquals(1, messageList.size()),
+                () -> assertEquals("Hello World from Spring Data JPA", messageList.get(messageList.size() - 1).getText())
+        );
+    }
+
 }
