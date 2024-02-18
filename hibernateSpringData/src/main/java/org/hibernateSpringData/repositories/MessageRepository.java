bc7bd0df5d7cd59288332f26cda98fd636f3106a
ShrayankM <shraymist@gmail.com>
2024-02-18 10:06:25 +0530

    added repository for entity with JPA extension

--- hibernateSpringData/src/main/java/org/hibernateSpringData/repositories/MessageRepository.java
+++ hibernateSpringData/src/main/java/org/hibernateSpringData/repositories/MessageRepository.java
@@ -1,5 +1,6 @@
-package org.hibernateSpringData;
+package org.hibernateSpringData.repositories;
 
+import org.hibernateSpringData.Message;
 import org.springframework.data.repository.CrudRepository;
 
 public interface MessageRepository extends CrudRepository<Message, Long> {
