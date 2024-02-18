972e7d30386790b56e81ec6aa595ebc0dc8a59d8
ShrayankM <shraymist@gmail.com>
2024-02-18 10:05:42 +0530

    added configuration for hibernate, spring-data and mysql DB

--- hibernateSpringData/src/main/java/org/hibernateSpringData/SpringDataConfiguration.java
+++ hibernateSpringData/src/main/java/org/hibernateSpringData/SpringDataConfiguration.java
@@ -1,2 +1,64 @@
-package org.hibernateSpringData;public class SpringDataConfiguration {
+package org.hibernateSpringData;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.Database;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+
+import javax.persistence.EntityManagerFactory;
+import javax.sql.DataSource;
+import java.util.Properties;
+
+@Configuration
+@EnableJpaRepositories ("org.hibernateSpringData.repositories")
+public class SpringDataConfiguration {
+
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+
+        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+        dataSource.setUrl("jdbc:mysql://localhost:3306/hibernateSpringData?serverTimezone=UTC");
+        dataSource.setUsername("root");
+        dataSource.setPassword("Ananya@ak47onetaps");
+
+        return dataSource;
+    }
+
+    @Bean
+    public JpaTransactionManager transactionManager(EntityManagerFactory emf) {
+        return new JpaTransactionManager(emf);
+    }
+
+    @Bean
+    public JpaVendorAdapter jpaVendorAdapter() {
+        HibernateJpaVendorAdapter jpaVendorAdapter = new HibernateJpaVendorAdapter();
+
+        jpaVendorAdapter.setDatabase(Database.MYSQL);
+        jpaVendorAdapter.setShowSql(true);
+
+        return jpaVendorAdapter;
+    }
+
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+        LocalContainerEntityManagerFactoryBean entityManagerFactoryBean = new LocalContainerEntityManagerFactoryBean();
+
+        entityManagerFactoryBean.setDataSource(dataSource());
+
+        Properties properties = new Properties();
+        properties.put("hibernate.hbm2ddl.auto", "create");
+
+        entityManagerFactoryBean.setJpaProperties(properties);
+        entityManagerFactoryBean.setJpaVendorAdapter(jpaVendorAdapter());
+        entityManagerFactoryBean.setPackagesToScan("org.hibernateSpringData");
+
+        return entityManagerFactoryBean;
+
+    }
 }
